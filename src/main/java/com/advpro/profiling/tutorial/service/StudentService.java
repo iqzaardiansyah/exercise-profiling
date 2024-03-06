cd9b58f91c6fa5e3716eb68dd5f1683ac0fd8a78
iqzaardiansyah <iqzaardiansyah@gmail.com>
2024-03-06 10:38:47 +0700

    [Refactoring] - Optimizing method findStudentWithHighestGpa and joinStudentNames.

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -5,9 +5,11 @@ import com.advpro.profiling.tutorial.model.StudentCourse;
 import com.advpro.profiling.tutorial.repository.StudentCourseRepository;
 import com.advpro.profiling.tutorial.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -34,23 +36,14 @@ public class StudentService {
     }
 
     public Optional<Student> findStudentWithHighestGpa() {
-        List<Student> students = studentRepository.findAll();
-        Student highestGpaStudent = null;
-        double highestGpa = 0.0;
-        for (Student student : students) {
-            if (student.getGpa() > highestGpa) {
-                highestGpa = student.getGpa();
-                highestGpaStudent = student;
-            }
-        }
-        return Optional.ofNullable(highestGpaStudent);
+        return Optional.ofNullable(studentRepository.findAll().get(0));
     }
 
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Student student : students) {
-            result += student.getName() + ", ";
+            result.append(student.getName()).append(", ");
         }
         return result.substring(0, result.length() - 2);
     }
